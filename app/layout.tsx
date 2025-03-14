4762b596054b49b3893ce1af42bdb281f8764706
markshawn2020 <shawninjuly@gmail.com>
2025-03-15 01:35:56 +0800

    init account

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import './globals.css';
-import type { Metadata, Viewport } from 'next';
-import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
+import type { Metadata, Viewport } from 'next';
+import { Manrope } from 'next/font/google';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
@@ -26,8 +26,9 @@ export default function RootLayout({
     <html
       lang="en"
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
+      suppressHydrationWarning
     >
-      <body className="min-h-[100dvh] bg-gray-50">
+      <body className="min-h-[100dvh] bg-gray-50" suppressHydrationWarning>
         <UserProvider userPromise={userPromise}>{children}</UserProvider>
       </body>
     </html>
